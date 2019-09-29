9b5f48d1355fd9818b6a9953ca5042b972d696d2
Luis F M Alves <luis.projensino@gmail.com>
2019-09-29 11:49:26 -0300

    feat: new named methods guessed? and island

--- islands_engine/lib/coordinate.ex
+++ islands_engine/lib/coordinate.ex
@@ -6,4 +6,12 @@ defmodule IslandsEngine.Coordinate do
 	def start_link() do 
 		Agent.start_link(fn -> %Coordinate{} end)
 	end
+
+	def guessed?(coordinate) do 
+		Agent.get(coordinate, fn state -> state.guessed? end)
+	end
+
+	def island(coordinate) do 
+		Agent.get(coordinate, fn state -> state.in_island end)
+	end
 end
