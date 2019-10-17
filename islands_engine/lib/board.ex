0ebdaca5478cb4d2da1fb18e35969129e9144e2b
LFM Alves <luis.projensino@gmail.com>
2019-10-17 18:53:20 -0300

    feat: @letters and @numbers in board.ex

--- islands_engine/lib/board.ex
+++ islands_engine/lib/board.ex
@@ -1,6 +1,9 @@
 defmodule IslandsEngine.Board do
   alias IslandsEngine.Coordinate
 
+  @letters ~W(a b c d e f g h i j)
+  @numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
   def start_link do
     Agent.start_link(fn -> %{} end)
   end
