57a09559eff9de878894063d34c367f3a0089b6e
LFM Alves <luis.projensino@gmail.com>
2019-10-18 14:20:20 -0300

    feat: monadic function start_link for Player

--- islands_engine/lib/player.ex
+++ islands_engine/lib/player.ex
@@ -2,4 +2,10 @@ defmodule IslandsEngine.Player do
   alias IslandsEngine.{Board, Coordinate, IslandSet, Player}
 
   defstruct name: :none, board: :none, island_set: :none
+
+  def start_link(name \\ :none) do
+    {:ok, board} = Board.start_link()
+    {:ok, island_set} = IslandSet.start_link()
+    Agent.start_link(fn -> %Player{board: board, island_set: island_set, name: name} end)
+  end
 end
