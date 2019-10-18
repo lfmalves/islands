5c30560c126cd6d3b4c454a798a6090adc2f8038
LFM Alves <luis.projensino@gmail.com>
2019-10-18 11:23:46 -0300

    feat: start_link function on island_set.ex

--- islands_engine/lib/island_set.ex
+++ islands_engine/lib/island_set.ex
@@ -2,4 +2,8 @@ defmodule IslandsEngine.IslandSet do
   alias IslandsEngine.{Island, IslandSet}
 
   defstruct atoll: :none, dot: :none, l_shape: :none, s_shape: :none, square: :none
+
+  def start_link() do
+    Agent.start_link(fn -> %IslandSet{} end)
+  end
 end
