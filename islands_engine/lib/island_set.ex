b350174ffe41ef685752e959368f5c3949009218
LFM Alves <luis.projensino@gmail.com>
2019-10-18 11:25:50 -0300

    fix: improvement on start_link of island_set

--- islands_engine/lib/island_set.ex
+++ islands_engine/lib/island_set.ex
@@ -4,6 +4,6 @@ defmodule IslandsEngine.IslandSet do
   defstruct atoll: :none, dot: :none, l_shape: :none, s_shape: :none, square: :none
 
   def start_link() do
-    Agent.start_link(fn -> %IslandSet{} end)
+    Agent.start_link(fn -> initialized_set() end)
   end
 end
