a13344ec469706a075100230a9750559ab4bf4fb
Luis F M Alves <luis.projensino@gmail.com>
2019-10-19 12:45:26 -0300

    fix: exclusion of dyadic function handle_info for Game

--- islands_engine/lib/game.ex
+++ islands_engine/lib/game.ex
@@ -1,11 +1,6 @@
 defmodule IslandsEngine.Game do
   use GenServer
 
-  def handle_info(:first, state) do
-    IO.puts("This message has been handled by handle_info/2, matching on :first.")
-    {:noreply, state}
-  end
-
   def handle_call(:demo, _from, state) do
     {:reply, state, state}
   end
