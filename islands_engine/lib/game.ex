dacdd3c388933f8e1355acaf132a5d50e30a6e97
Luis F M Alves <luis.projensino@gmail.com>
2019-10-19 13:20:10 -0300

    feat: monadic function init for Game

--- islands_engine/lib/game.ex
+++ islands_engine/lib/game.ex
@@ -9,6 +9,12 @@ defmodule IslandsEngine.Game do
     GenServer.start_link(__MODULE__, name)
   end
 
+  def init(name) do
+    {:ok, player1} = Player.start_link(name)
+    {:ok, player2} = Player.start_link()
+    {:ok, %Game{player1: player1, player2: player2}}
+  end
+
   def handle_call(:demo, _from, state) do
     {:reply, state, state}
   end
