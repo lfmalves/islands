406719d03761194b607b38c7550aa3b51bb40386
lfmalves <luis.projensino@gmail.com>
2020-09-02 09:58:43 -0300

    missing terminator end

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -5,13 +5,14 @@ defmodule IslandsEngine.Island do
   defstruct [:coordinates, :hit_coordinates]
 
   def new(type, %Coordinate{} = upper_left) do
-    with [_|_] = offsets <- offsets(type),
-	 %MapSet{} = coordinates <- add_coordinates(offsets, upper_left) do
+    with [_ | _] = offsets <- offsets(type),
+         %MapSet{} = coordinates <- add_coordinates(offsets, upper_left) do
       {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
     else
       error -> error
     end
-  
+  end
+
   defp offsets(:square), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
   defp offsets(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
   defp offsets(:dot), do: [{0, 0}]
